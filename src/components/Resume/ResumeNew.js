fa3e406f8e091266802d01b231cc25610fab2dba
Stefano Fabiano <108979350+Awalsia@users.noreply.github.com>
2025-07-16 14:26:39 +0200

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,32 +1,41 @@
 import React from "react";
-import { Viewer } from "@react-pdf-viewer/core";
-import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
-import "@react-pdf-viewer/core/lib/styles/index.css";
-import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 import { AiOutlineDownload } from "react-icons/ai";
 
-const pdf = "/Stefano_Fabiano_CV.pdf";
+// Import viewer
+import { Viewer } from "@react-pdf-viewer/core";
+import "@react-pdf-viewer/core/lib/styles/index.css";
+
+// PDF path (in /public)
+const pdfUrl = "/Stefano_Fabiano_CV.pdf";
 
 function ResumeNew() {
-  const defaultLayoutPluginInstance = defaultLayoutPlugin();
-
   return (
     <div>
       <Container fluid className="resume-section">
         <Particle />
-        <Row style={{ justifyContent: "center", marginBottom: "20px" }}>
-          <Button variant="primary" href={pdf} target="_blank" style={{ maxWidth: "250px" }}>
+
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button variant="primary" href={pdfUrl} target="_blank" style={{ maxWidth: "250px" }}>
             <AiOutlineDownload />
             &nbsp;Download CV
           </Button>
         </Row>
 
-        <div style={{ height: "90vh" }}>
-          <Viewer fileUrl={pdf} plugins={[defaultLayoutPluginInstance]} />
-        </div>
+        <Row style={{ justifyContent: "center", marginTop: "30px" }}>
+          <div style={{ width: "80%", height: "800px" }}>
+            <Viewer fileUrl={pdfUrl} />
+          </div>
+        </Row>
+
+        <Row style={{ justifyContent: "center", marginTop: "20px" }}>
+          <Button variant="primary" href={pdfUrl} target="_blank" style={{ maxWidth: "250px" }}>
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button>
+        </Row>
       </Container>
     </div>
   );
