d33f7455e336d14238ed9e4e84cd0e9c0ce413b0
Stefano Fabiano <108979350+Awalsia@users.noreply.github.com>
2025-07-16 12:16:43 +0200

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -5,10 +5,8 @@ import Particle from "../Particle";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 
-// Imposta il worker per react-pdf
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-// Percorso pubblico del PDF (deve trovarsi in /public)
 const pdf = "/Stefano_Fabiano_CV.pdf";
 
 function ResumeNew() {
@@ -27,47 +25,32 @@ function ResumeNew() {
     <div>
       <Container fluid className="resume-section">
         <Particle />
-
-        {/* Pulsante in alto */}
         <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
+          <Button variant="primary" href={pdf} target="_blank" style={{ maxWidth: "250px" }}>
             <AiOutlineDownload />
             &nbsp;Download CV
           </Button>
         </Row>
 
-        {/* Visualizzatore PDF */}
         <Row className="resume d-flex flex-column align-items-center">
           <Document
             file={pdf}
             onLoadSuccess={onDocumentLoadSuccess}
             className="d-flex flex-column align-items-center"
           >
-            {numPages &&
-              Array.from({ length: numPages }, (_, index) => (
-                <Page
-                  key={`page_${index + 1}`}
-                  pageNumber={index + 1}
-                  scale={width > 786 ? 1.7 : 0.6}
-                  className="my-3"
-                />
-              ))}
+            {Array.from({ length: numPages }, (_, index) => (
+              <Page
+                key={`page_${index + 1}`}
+                pageNumber={index + 1}
+                scale={width > 786 ? 1.7 : 0.6}
+                className="my-3"
+              />
+            ))}
           </Document>
         </Row>
 
-        {/* Pulsante in basso */}
         <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
+          <Button variant="primary" href={pdf} target="_blank" style={{ maxWidth: "250px" }}>
             <AiOutlineDownload />
             &nbsp;Download CV
           </Button>
