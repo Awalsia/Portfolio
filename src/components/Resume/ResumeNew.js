3c38c0367c2ca3b831c494e13cdd96db00c76592
Stefano Fabiano <108979350+Awalsia@users.noreply.github.com>
2025-07-16 15:10:11 +0200

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,13 +1,9 @@
 import React from "react";
 import { Container, Row } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import { AiOutlineDownload } from "react-icons/ai";
 
-// Percorso del file immagine (mettilo in /public)
+// Percorso dell'immagine del CV (deve stare nella cartella public)
 const imageCV = "/Stefano_Fabiano_CV.png";
-// Percorso del file PDF per il download (opzionale)
-const pdfCV = "/Stefano_Fabiano_CV.pdf";
 
 function ResumeNew() {
   return (
@@ -15,26 +11,17 @@ function ResumeNew() {
       <Container fluid className="resume-section">
         <Particle />
 
-        <Row style={{ justifyContent: "center", position: "relative", marginBottom: "20px" }}>
-          <Button variant="primary" href={pdfCV} target="_blank" style={{ maxWidth: "250px" }}>
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
-        </Row>
-
-        <Row style={{ justifyContent: "center" }}>
+        <Row style={{ justifyContent: "center", marginBottom: "40px" }}>
           <img
             src={imageCV}
             alt="Stefano Fabiano CV"
-            style={{ maxWidth: "90%", height: "auto", border: "1px solid #ccc", boxShadow: "0px 0px 10px rgba(0,0,0,0.2)" }}
+            style={{
+              maxWidth: "90%",
+              height: "auto",
+              border: "1px solid #ccc",
+              boxShadow: "0px 0px 10px rgba(0,0,0,0.2)",
+            }}
           />
-        </Row>
-
-        <Row style={{ justifyContent: "center", marginTop: "20px" }}>
-          <Button variant="primary" href={pdfCV} target="_blank" style={{ maxWidth: "250px" }}>
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
         </Row>
       </Container>
     </div>
